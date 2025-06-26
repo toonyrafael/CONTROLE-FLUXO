1b7a3635284f7760ea0a43001a9426462fddc74e
toonyrafael <toonyrafael@gmail.com>
2025-06-25 23:39:36 -0300

    Adicionado Classe de Resultado Escolar

--- src/CaixaEletronico.java
+++ src/CaixaEletronico.java
@@ -1,12 +1,17 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         double saldo = 25.0;
-        double valorSolicitado = 26.0;
+        double valorSolicitado = 5.0;
 
-        if(valorSolicitado < saldo)
-        saldo = saldo - valorSolicitado;
+        if(valorSolicitado < saldo){
+            saldo = saldo - valorSolicitado;
+            System.out.println("Novo Saldo "+ saldo);
+        }
+            
+        else
+            System.out.println("Saldo Insuficiente");
 
-        System.out.println(saldo);
+        
   }
   
 }
