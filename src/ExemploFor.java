1a26246acda251e25296c726d4734e35bb5186c3
toonyrafael <toonyrafael@gmail.com>
2025-06-26 11:36:50 -0300

    Exemplo FOR Criado

---
+++ src/ExemploFor.java
@@ -0,0 +1,8 @@
+public class ExemploFor {
+  public static void main(String[] args) {
+    for (int carneiro = 1; carneiro <=20; carneiro++){
+        System.out.println("Contando Carneiros " + carneiro);
+    }
+    System.out.println("João Dormiu");
+  }
+}
