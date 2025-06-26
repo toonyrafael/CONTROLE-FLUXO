5af88cbb54fe9e34378c86b4e5c2306f63434729
toonyrafael <toonyrafael@gmail.com>
2025-06-25 23:45:38 -0300

    Adicionado mais uma condição IF

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,9 +1,12 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
-      int nota = 7;
+      int nota = 6;
 
     if (nota >= 7)
       System.out.println("Aprovado");
+
+    else if (nota >= 5 && nota < 7)
+      System.out.println("Prova de Recuperação");
 
     else
       System.out.println("Reprovado");
