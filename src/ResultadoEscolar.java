11331b9e78b18ff65a03e845f4c0ac5c3e408728
toonyrafael <toonyrafael@gmail.com>
2025-06-25 23:52:40 -0300

    Modificado a Estrutura do IF para mais simples no Resultado Escolar

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -1,14 +1,8 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
-      int nota = 6;
-
-    if (nota >= 7)
-      System.out.println("Aprovado");
-
-    else if (nota >= 5 && nota < 7)
-      System.out.println("Prova de Recuperação");
-
-    else
-      System.out.println("Reprovado");
+      int nota = 3;
+      String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+      System.out.println(resultado);
+      
   }
 }
